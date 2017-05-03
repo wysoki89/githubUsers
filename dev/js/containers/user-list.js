04b06f2575f66b95afecb40390605a0bc1181384
wysoki89 <twysokinski@gmail.com>
2017-05-03 17:34:24 +0200

    autocomplete init

--- dev/js/containers/user-list.js
+++ dev/js/containers/user-list.js
@@ -3,6 +3,9 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as userActions from '../actions/user';
 import Select from 'react-select';
+import AutoComplete from 'material-ui/AutoComplete';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
 
 @connect((store) => {
     return {
@@ -14,7 +17,10 @@ export default class UserList extends Component {
     onInput(e) {
         e.preventDefault();
         this.props.dispatch(userActions.fetchUsers(`https://api.github.com/search/users?q=${e.target.value} in:login`))
-     }
+    }
+    getUsers () {
+            return [1,2,3]
+        }
     render() {
         const { users } = this.props;
         const onSelect = () => {
@@ -24,10 +30,13 @@ export default class UserList extends Component {
             window.open(selectedUser.html_url);
         }
         const onEnter = (e) => {
-            if(e.key === "Enter"){
+            if (e.key === "Enter") {
                 e.preventDefault();
                 onSelect()
             }
+        const getUsers = () =>{
+            return [1,2,3]
+        }
         }
         let input;
         return (
@@ -39,6 +48,12 @@ export default class UserList extends Component {
                     {users.map(user => <option key={user.id} value={user.login} />)}
                 </datalist>
                 <input type="button" value="Select" onClick={onSelect} />
+                <MuiThemeProvider>
+                    <AutoComplete
+                        hintText="Type anything"
+                        dataSource={this.getUsers()}
+                    />
+                </MuiThemeProvider>
             </div>
         )
     }
